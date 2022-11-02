923dc37fcaad61ae916b48ce5a9cecdf0625e0c6
anikkumarsaha <anikkumarofficial08@gmail.com>
2022-11-02 10:36:19 +0600

    Mid Assignment Done

---
+++ ZeroHunger/Controllers/EmployeeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.DB;
+
+namespace ZeroHunger.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var db = new ZeroHungerEntities1();
+            return View(db.Employees.ToList());
+        }
+
+
+
+        public ActionResult ShowAssignedTasks(int id)
+        {
+            Session["EmpId"] = id;
+            var db = new ZeroHungerEntities1();
+            var tasks = (from t in db.EmployeeCollectRequests
+                         where t.EmployeeId == id
+                         select t).ToList();
+            return View(tasks);
+        }
+
+
+
+        public ActionResult ShowPendingTasks()
+        {
+            var id = Int32.Parse(Session["EmpId"].ToString());
+            var db = new ZeroHungerEntities1();
+            var tasks = (from t in db.EmployeeCollectRequests
+                         where t.EmployeeId == id
+                         select t.CollectRequestId);
+
+            var ReqTasks = db.CollectRequests;
+
+            List<CollectRequest> collectList = new List<CollectRequest>();
+
+            foreach (var item in db.CollectRequests)
+            {
+                foreach (var i in tasks)
+                {
+                    if(item.Id == i && item.Status == "Assigned")
+                    {
+                        collectList.Add(item);
+                    }
+                }
+            }
+
+            return View(collectList);
+        }
+
+
+        [HttpGet]
+        public ActionResult Complete(int id)
+        {
+            var db = new ZeroHungerEntities1();
+            var ext = (from cs in db.CollectRequests
+                       where cs.Id == id
+                       select cs).SingleOrDefault();
+            return View(ext);
+        }
+        [HttpPost]
+        public ActionResult Complete(CollectRequest c)
+        {
+            var db = new ZeroHungerEntities1();
+            var ext = (from cs in db.CollectRequests
+                       where cs.Id == c.Id
+                       select cs).SingleOrDefault();
+            ext.Status = "Completed";
+            ext.RestaurantId = ext.RestaurantId;
+            db.SaveChanges();
+
+            var id = Int32.Parse(Session["EmpId"].ToString());
+
+            var ex = (from cs in db.Employees
+                       where cs.Id == id
+                       select cs).SingleOrDefault();
+            ex.TotalCompletedRequests = ex.TotalCompletedRequests+1;
+            db.SaveChanges();
+
+            var ngo = db.NGOs.SingleOrDefault();
+            ngo.TotalCompletedRequests = ngo.TotalCompletedRequests + 1;
+            db.SaveChanges();
+
+            var Emid = Int32.Parse(Session["EmpId"].ToString());
+
+            return RedirectToAction("ShowPendingTasks");
+        }
+    }
+}
---
+++ ZeroHunger/Controllers/RestaurantController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeroHunger.DB;
+
+namespace ZeroHunger.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            var db = new ZeroHungerEntities1();
+            return View(db.Restaurants.ToList());
+        }
+
+
+
+        [HttpGet]
+        public ActionResult AllCollectRequest(int id)
+        {
+            Session["ResID"] = id;
+            var db = new ZeroHungerEntities1();
+            var Requests = (from r in db.CollectRequests
+                            where r.RestaurantId == id
+                            select r).ToList();
+            return View(Requests);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult PendingCollectRequest()
+        {
+            var ResId = Int32.Parse(Session["ResId"].ToString());
+            var db = new ZeroHungerEntities1();
+            var Requests = (from r in db.CollectRequests
+                            where r.RestaurantId == ResId && r.Status == "Requested"
+                            select r).ToList();
+            return View(Requests);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var db = new ZeroHungerEntities1();
+            var ext = (from cs in db.FoodDetails
+                       where cs.Id == id
+                       select cs).SingleOrDefault();
+            return View(ext);
+        }
+        [HttpPost]
+        public ActionResult Edit(FoodDetail c)
+        {
+            var db = new ZeroHungerEntities1();
+            var ext = (from cs in db.FoodDetails
+                       where cs.Id == c.Id
+                       select cs).SingleOrDefault();
+            ext.Name = c.Name;
+            ext.Amount = c.Amount;
+            ext.CollectRequestsId = ext.CollectRequestsId;
+            db.SaveChanges();
+            return RedirectToAction("PendingCollectRequest");
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var db = new ZeroHungerEntities1();
+            var ext = (from cs in db.FoodDetails
+                       where cs.Id == id
+                       select cs).SingleOrDefault();
+            return View(ext);
+        }
+        [HttpPost]
+        public ActionResult Delete(FoodDetail c)
+        {
+            var db = new ZeroHungerEntities1();
+            var ext = (from cs in db.FoodDetails
+                       where cs.Id == c.Id
+                       select cs).SingleOrDefault();
+            var CollId = ext.CollectRequestsId;
+            db.FoodDetails.Remove(ext);
+            db.SaveChanges();
+
+            var exxt = (from css in db.CollectRequests
+                       where css.Id == CollId
+                        select css).SingleOrDefault();
+            db.CollectRequests.Remove(exxt);
+            db.SaveChanges();
+            return RedirectToAction("PendingCollectRequest");
+        }
+
+
+
+        [HttpGet]
+        public ActionResult PlaceNewRequest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PlaceNewRequest(FoodDetail cs)
+        {
+            //Ekta CollectRequest er under e multiple Order place krbo pore.
+
+            var db = new ZeroHungerEntities1();
+
+            var ResId = Int32.Parse(Session["ResId"].ToString());
+            db.CollectRequests.Add(new CollectRequest()
+            {
+                Status = "Requested",
+                RestaurantId = ResId,
+            });
+
+            db.SaveChanges();
+
+            var CollectReq = db.CollectRequests.ToList().LastOrDefault();
+
+            db.FoodDetails.Add(new FoodDetail()
+            {
+                Name = cs.Name,
+                Amount = cs.Amount,
+                CollectRequestsId = CollectReq.Id,
+            });
+            db.SaveChanges();
+            return RedirectToAction("AllCollectRequest", new { id = ResId});
+        }
+    }
+}
